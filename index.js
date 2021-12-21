ee50a3795e25b95ee64ee93b76f1b02a293e8038
Lucas Albuquerque Ramos <lucasalbuquerque5412@gmail.com>
2021-12-21 21:09:04 +0000

    including jquery

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ router.use(express.urlencoded({extended: true})); //We allow the data sent from 
 router.use(express.json()); //We include support for JSON
 
 
-router.get('/', function(req, res) {
+router.get('/get/html', function(req, res) {
 
     res.writeHead(200, {'Content-Type' : 'text/html'});
 
