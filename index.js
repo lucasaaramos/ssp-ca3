80b90ce96d3a77ed79114652836a837fe0bd895f
Lucas Albuquerque Ramos <lucasalbuquerque5412@gmail.com>
2021-12-21 18:20:46 +0000

    update

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-//these are the dependences
-const   http = require('http'), 
-        path = require('path'), 
-        express = require('express'), 
-        fs = require('fs'), 
-        xmlParse = require('xslt-processor').xmlParse, 
-        xsltProcess = require('xslt-processor').xsltProcess, 
-        xml2js = require('xml2js'); 
- 
-const   router = express(); 
-const   server = http.createServer(router);
+const   http = require('http'), //This module provides the HTTP server functionalities
+        path = require('path'), //The path module provides utilities for working with file and directory paths
+        express = require('express'), //This module allows this app to respond to HTTP requests, defines the routing and renders back the required content
+        fs = require('fs'), //This module allows to work with the file system: read and write files back
+        xmlParse = require('xslt-processor').xmlParse, //This module allows to work with XML files
+        xsltProcess = require('xslt-processor').xsltProcess, //The same module allows us to uitlise XSL Transformations
+        xml2js = require('xml2js'); //This module does XML <-> JSON conversion
 
-router.use(express.static(path.resolve(__dirname,'views'))); //We serve static content from "views" folder
-router.use(express.urlencoded({extended: true})); //We allow the data sent from the client to be encoded in a URL targeting our end point
-router.use(express.json()); //We include support for JSON
+const   router = express(), 
+        server = http.createServer(router);
 
 
-router.get('/', function(req, res) {
+router.get('/get/html', function(req, res) {
 
     res.writeHead(200, {'Content-Type' : 'text/html'});
 
